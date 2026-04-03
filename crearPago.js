642f49b45fbf3fa3dcaa5c9c02167903a99068a2
vngambino77-ui <vngambino77@gmail.com>
2026-04-03 17:38:34 -0300

    Add files via upload

--- crearPago.js
+++ crearPago.js
@@ -1,35 +1,24 @@
 import mercadopago from "mercadopago";
 
 mercadopago.configure({
-  access_token: APP_USR-2728717194982261-032915-732e9a841643b7a4b6636b5255120cbb-3297989181
+  access_token:APP_USR-2728717194982261-032915-732e9a841643b7a4b6636b5255120cbb-3297989181
 });
 
-export default async function handler(req, res) {
+export default async function handler(req, res){
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método no permitido" });
-  }
+  const { fotos, precio, titulo } = req.body;
 
-  try {
+  const preference = {
+    items: fotos.map((f,i)=>({
+      title: `${titulo} Foto ${i+1}`,
+      quantity: 1,
+      unit_price: precio
+    }))
+  };
 
-    const { items } = req.body;
+  const response = await mercadopago.preferences.create(preference);
 
-    const preference = await mercadopago.preferences.create({
-      items,
-      back_urls: {
-        success: "https://rinascere-web.vercel.app/evento.html",
-        failure: "https://rinascere-web.vercel.app/evento.html",
-        pending: "https://rinascere-web.vercel.app/evento.html"
-      },
-      auto_return: "approved"
-    });
-
-    res.status(200).json({
-      id: preference.body.id
-    });
-
-  } catch (error) {
-    console.error("ERROR MP:", error);
-    res.status(500).json({ error: error.message });
-  }
+  res.json({
+    init_point: response.body.init_point
+  });
 }
