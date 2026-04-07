c2aec9ab6fa2f00b0ed63234315cf2a768e30ae2
vngambino77-ui <vngambino77@gmail.com>
2026-04-07 16:56:43 -0300

    imagenesroute.js

--- api/imagenesroute.js
+++ api/imagenesroute.js
@@ -1,23 +1,24 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const carpeta = searchParams.get("carpeta");
-
-  const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export async function GET() {
+  const API_KEY = process.env.GOOGLE_API_KEY;
+  const FOLDER_ID = process.env.GOOGLE_FOLDER_ID;
 
   try {
     const res = await fetch(
-      `https://res.cloudinary.com/${CLOUD_NAME}/image/list/${carpeta}.json`
+      `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}&fields=files(id,name,mimeType)`
     );
 
     const data = await res.json();
 
-    const imagenes = data.resources.map(
-      (img) => `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/${img.public_id}.jpg`
-    );
+    const images = data.files
+      .filter(file => file.mimeType.includes("image"))
+      .map(file => 
+        `https://drive.google.com/uc?export=view&id=${file.id}`
+      );
 
-    return NextResponse.json(imagenes);
+    return NextResponse.json(images);
+
   } catch (error) {
     return NextResponse.json({ error: "Error cargando imágenes" }, { status: 500 });
   }
