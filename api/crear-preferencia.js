df60a0eedb2f64936ad0a1c5f6c23176e312fe41
veronica <vngambino77@gmail.com>
2026-04-11 19:14:09 -0300

    crearpreferencia.js

--- api/crear-preferencia.js
+++ api/crear-preferencia.js
@@ -1,3 +1,4 @@
+// api/crear-preferencia.js
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Método no permitido' });
