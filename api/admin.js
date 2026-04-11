2cef8d1d7e947d9eb299a7a16ff1aa6b03a6c96c
veronica <vngambino77@gmail.com>
2026-04-11 18:18:40 -0300

    Add files via upload

--- api/admin.js
+++ api/admin.js
@@ -1,3 +1,5 @@
+// admin.js - Script actualizado para el panel de administración
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { 
     getFirestore, 
