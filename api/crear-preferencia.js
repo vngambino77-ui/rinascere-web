a4239c95ea4275b243b68ade2c07e41b6194e0ff
veronica <vngambino77@gmail.com>
2026-04-14 22:00:57 -0300

    crear-preferencia.js

--- api/crear-preferencia.js
+++ api/crear-preferencia.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    // CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
